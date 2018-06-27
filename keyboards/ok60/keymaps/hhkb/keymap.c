ef29340919b81e6f9e36346d21f532e37b256d5a
oflow <github@oflow.me>
2018-06-27 22:26:30 +0900

    add layer3, if layer3 then rgblight(purple)

--- keyboards/ok60/keymaps/hhkb/keymap.c
+++ keyboards/ok60/keymaps/hhkb/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |----------------------------------------------------------------------------------------|
      * | MO(1) |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |     Enter    |
      * |----------------------------------------------------------------------------------------|
-     * | Shift  |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |  IME ON   | MO(2) |
+     * | IME OFF |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |  IME ON  | MO(2) |
      * `----------------------------------------------------------------------------------------'
      *         | Ctl |  Alt  |               Shift & Space               |  Alt  | Ctl |
      *         `-----------------------------------------------------------------------'
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESCGR, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,  KC_GRV,  KC_BSDEL,
         KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS,
         MO(1),    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN,  KC_QUOT,  XXXXX,   KC_ENT,
-        KC_LSFT,  XXXXX,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  XXXXX,   IME_ON,  MO(2),
+        IME_OFF,  XXXXX,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  XXXXX,   IME_ON,  MO(2),
         XXXXX,   KC_LCTL, KC_LALT,                SFT_T(KC_SPC),          XXXXX,  KC_RALT,  KC_RCTL,  XXXXX),
 
     KEYMAP( // MO(1) : left side modifer
@@ -50,8 +50,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9, KC_F10, KC_F11, KC_F12, _____, LCTL(LALT(KC_DEL)),
         _____,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _____, _____,  _____,  _____,  _____,
         KC_CAPS, RGB_PLN, RGB_BRT, _____,   _____,   _____,   _____,   _____,   _____,   _____, _____,  _____,  _____,  _____,
-        _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____, _____,  _____,  _____,  IME_OFF, _____,
-        _____,   KC_APP,  _____,                     KC_SPC,                    _____,   _____, KC_APP, _____)
+        _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____, _____,  _____,  _____,  TG(3), _____,
+        _____,   KC_APP,  _____,                     KC_SPC,                    _____,   _____, KC_APP, _____),
+    KEYMAP(
+        KC_ESCGR, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,  KC_GRV,  KC_BSDEL,
+        KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS,
+        MO(1),    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN,  KC_QUOT,  XXXXX,   KC_ENT,
+        KC_LSFT,  XXXXX,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  XXXXX,   IME_ON,  TG(3),
+
+        XXXXX,   KC_LCTL, KC_LALT,                SFT_T(KC_SPC),          XXXXX,  KC_RALT,  KC_RCTL,  XXXXX)
+
+
 };
 
 void matrix_init_user(void) {
@@ -59,7 +68,23 @@ void matrix_init_user(void) {
 
 void matrix_scan_user(void) {
 }
+uint32_t layer_state_set_user(uint32_t state) {
+    switch (biton32(state)) {
+    case 0:
+    case 1:
+    case 2:
+        rgblight_setrgb (0xFF,  0x00, 0x00);
+        break;
+    case 3:
+        rgblight_setrgb (0x7A,  0x00, 0xFF);
+        break;
+    default:
+        rgblight_setrgb (0xFF,  0x00, 0x00);
+        break;
+    }
+    return state;
 
+}
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     bool shift_pressed = get_mods() & MOD_BIT(KC_LSFT);
     bool ctrl_pressed  = get_mods() & MOD_BIT(KC_RCTL);
