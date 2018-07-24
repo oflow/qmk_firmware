18b431111e23c8cf7cb7cf140dad499630d91496
oflow <github@oflow.me>
2018-07-24 23:26:28 +0900

    fetch qmk/qmk_firmware, rename LAYOUT_60_hhkb

--- keyboards/ok60/keymaps/hhkb/keymap.c
+++ keyboards/ok60/keymaps/hhkb/keymap.c
@@ -8,14 +8,14 @@
 #define _R     2
 #define _GAME  3  // for FPS Game (need LShift)
 
-// too long
-#define ALT_SFT_A   LSFT(LALT(KC_A))
-#define CTL_ALT_DEL LCTL(LALT(KC_DEL))
-#define GUI_TAB     LGUI(KC_TAB)
-#define GUI_W       LGUI(KC_W)
-#define GUI_SFT_S   LGUI(LSFT(KC_S))
-#define SFT_INS     LSFT(KC_INS)
-#define CTL_N       LCTL(KC_N)
+// short modifier: S(kc) = LSFT(kc) = (kc | QK_LSFT)
+#define C(kc)      (kc | QK_LCTL)
+#define A(kc)      (kc | QK_LALT)
+#define G(kc)      (kc | QK_LGUI)
+#define C_A(kc)    (kc | QK_LCTL | QK_LALT)
+#define S_C(kc)    (kc | QK_LSFT | QK_LCTL)
+#define S_A(kc)    (kc | QK_LSFT | QK_LALT)
+#define S_G(kc)    (kc | QK_LSFT | QK_LGUI)
 
 #define RGB_PLN RGB_MODE_PLAIN
 #define RGB_BRT RGB_MODE_BREATHE
@@ -25,25 +25,25 @@
 #endif
 
 enum custom_keycodes {
-    KC_ESCGR = SAFE_RANGE, // Esc, Shift: ~, Ctrl: `
-    KC_BSDEL, // BS,  Shift: Del
-    KC_M_F12, // F12, Shift: F11,     Ctrl: F13
+    KC_ESGR = SAFE_RANGE, // Esc, Shift: ~, Ctrl: `
+    KC_BSDL,  // BS,  Shift: Del
+    KC_MF12,  // F12, Shift: F11,     Ctrl: F13
     IME_ON,   // Tap: Ctrl+Shift+Ins, Hold: RCtrl
-    IME_OFF   // Tap: Ctrl+Ins,       Hold: LCtrl
+    IME_OFF,  // Tap: Ctrl+Ins,       Hold: LCtrl
+    IME_TG    // Tap: Ctrl_Space      Hold: LCtrl
 };
 
-static bool bsdel_shifted = false;
-static bool escgr_shifted = false;
-static bool escgr_ctrled = false;
+static bool mods_bsdl[2] = {0, 0};       // lsft, rsft
+static bool mods_esgr[4] = {0, 0, 0, 0}; // lsft, rsft, lctl, rctl
 
-static bool ctrl_interrupted[2] = {0, 0};
-static uint16_t scs_timer[2] = {0, 0};
+static bool ctrl_interrupted[3] = {0, 0, 0};
+static uint16_t scs_timer[3] = {0, 0, 0};
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ,----------------------------------------------------------------------------------------.
-     * | ESC |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |  `  | BS |
+     * | ESC |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  | F12 | BS |
      * |----------------------------------------------------------------------------------------|
      * | TAB  |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |    \    |
      * |----------------------------------------------------------------------------------------|
@@ -55,36 +55,61 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *         `----------------------------------------------------------------------'
      *
      */
-    [_BASE] = KEYMAP(
-        KC_ESCGR, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,  KC_M_F12, KC_BSDEL,
-        KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS,
-        MO(_L),   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN,  KC_QUOT,  XXX,     KC_ENT,
-        IME_OFF,  XXX,    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,   KC_SLSH,  XXX,     IME_ON,   MO(_R),
-        XXX,     KC_LALT, KC_LGUI,                SFT_T(KC_SPC),          XXX,     KC_RGUI, KC_RALT,  XXX),
+    [_BASE] = LAYOUT_60_hhkb(
+        KC_ESGR, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_MF12, KC_BSDL,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+        MO(_L),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, XXX,     KC_ENT,
+        IME_OFF, XXX,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXX,     IME_ON,  MO(_R),
+        XXX,     KC_LALT, KC_LGUI,                   LSFT_T(KC_SPC),                     XXX,     KC_RGUI, KC_RALT, XXX),
 
-    [_L] = KEYMAP( // MO(1) : left side modifer
-        RESET,    KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,  KC_INS,  KC_DEL,
-        GUI_TAB,  ___,    GUI_W,  ___,    ___,    ___,    ___,    ___,    SFT_INS, ___,     KC_PSCR,  KC_HOME,  KC_END,  KC_PIPE,
-        ___,      ALT_SFT_A, GUI_SFT_S, ___, ___, ___,    KC_LEFT,KC_DOWN,KC_UP,   KC_RGHT, ___,      ___,      ___,     ___,
-        KC_LCTL,  ___,    ___,    ___,    ___,    ___,    ___,    CTL_N,  ___,     KC_PGDN, KC_PGUP,  ___,      ___,     KC_RCTL, ___,
-        ___,      ___,    ___,                    ___,                    ___,     ___,     ___,      ___),
+    /*
+     * ,----------------------------------------------------------------------------------------.
+     * | RSET|  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 | Ins | Del|
+     * |----------------------------------------------------------------------------------------|
+     * | G-Tab|     | G-w |     |     |     |     |     |S-Ins|     | PrScr| Home | End |       |
+     * |----------------------------------------------------------------------------------------|
+     * |       |A-S-a|G-S-s|     |     |     |  ← |  ↓ |  ↑ |  → |     |     |     Enter    |
+     * |----------------------------------------------------------------------------------------|
+     * | LCtrl   |     |     |     |     |     | C-n |     | PgDn| PgUp|     |  RCtrl   | MO(R) |
+     * `----------------------------------------------------------------------------------------'
+     *         |     |        |             Shift & Space              |        |     |
+     *         `----------------------------------------------------------------------'
+     */
+    [_L] = LAYOUT_60_hhkb( // MO(1) : left side modifer
+        RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_DEL,
+        G(KC_TAB),___,    G(KC_W), ___,     ___,     ___,     ___,     ___,     S(KC_INS),___,    KC_PSCR, KC_HOME, KC_END,  KC_PIPE,
+        ___,     S_A(KC_A),S_G(KC_S),___,   ___,     ___,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, ___,     ___,     ___,     ___,
+        KC_LCTL, ___,     ___,     ___,     ___,     ___,     ___,     C(KC_N), ___,     KC_PGDN, KC_PGUP, ___,     ___,     KC_LCTL, ___,
+        ___,     ___,     ___,                       ___,                                ___,     ___,     ___,     ___),
 
-    [_R] = KEYMAP( // MO(2) : right side modifer
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9, KC_F10, KC_F11, KC_F12, ___,     CTL_ALT_DEL,
-        ___,     RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, ___,   ___,    ___,    ___,    ___,
-        KC_CAPS, RGB_PLN, RGB_BRT, ___,     ___,     ___,     ___,     ___,     ___,     ___,   ___,    ___,    ___,    ___,
-        ___,     ___,     ___,     ___,     ___,     ___,     ___,     ___,     ___,     ___,   ___,    ___,    ___,    TG(_GAME), ___,
-        ___,     KC_APP,  ___,                       LSFT(KC_SPC),              ___,     ___,   KC_APP, ___),
+    [_R] = LAYOUT_60_hhkb( // MO(2) : right side modifer
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  ___,     C_A(KC_DEL),
+        ___,     RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, ___,     ___,     ___,     ___,     ___,
+        KC_CAPS, RGB_PLN, RGB_BRT, ___,     ___,     ___,     ___,     ___,     ___,     ___,     ___,     ___,     ___,     ___,
+        ___,     ___,     ___,     ___,     ___,     ___,     ___,     ___,     ___,     ___,     ___,     ___,     ___,     TG(_GAME), ___,
+        ___,     KC_APP,  ___,                       S(KC_SPC),                          ___,     ___,     KC_APP,  ___),
 
-    [_GAME] = KEYMAP( // TG(3)
-        KC_ESCGR, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,  KC_GRV,  KC_BSDEL,
-        KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS,
-        MO(_L),   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN,  KC_QUOT,  XXX,     KC_ENT,
-        KC_LSFT,  XXX,    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,   KC_SLSH,  XXX,     IME_ON,  TG(_GAME),
+    /*
+     * ,----------------------------------------------------------------------------------------.
+     * | ESC |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  | F12 | BS |
+     * |----------------------------------------------------------------------------------------|
+     * | TAB  |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |    \    |
+     * |----------------------------------------------------------------------------------------|
+     * | Map   |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |     Enter    |
+     * |----------------------------------------------------------------------------------------|
+     * | LShift  |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |  IME TG  | TG(G) |
+     * `----------------------------------------------------------------------------------------'
+     *         | LCtl |  LAlt  |           RShift & Space              |  RGUI  | Alt |
+     *         `----------------------------------------------------------------------'
+     */
 
-        XXX,      KC_LCTL, KC_LALT,                SFT_T(KC_SPC),         XXX,     KC_RALT,  KC_RCTL,  XXX)
+    [_GAME] = LAYOUT_60_hhkb( // TG(3)
+        KC_ESGR, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_MF12,  KC_BSDL,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+        LT(_L,KC_M),KC_A, KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, XXX,     KC_ENT,
+        KC_LSFT, XXX,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXX,     IME_TG,   TG(_GAME),
 
-
+        XXX,      KC_LCTL, KC_LALT,                  RSFT_T(KC_SPC),                      XXX,     KC_RGUI, KC_RALT,  XXX)
 };
 
 void matrix_init_user(void) {
@@ -110,49 +135,77 @@ uint32_t layer_state_set_user(uint32_t state) {
 
 }
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    bool shift_pressed = get_mods() & MOD_BIT(KC_LSFT);
-    bool ctrl_pressed  = get_mods() & MOD_BIT(KC_RCTL);
+    bool lsft_pressed = get_mods() & MOD_BIT(KC_LSFT);
+    bool rsft_pressed = get_mods() & MOD_BIT(KC_RSFT);
+    bool lctl_pressed = get_mods() & MOD_BIT(KC_LCTL);
+    bool rctl_pressed = get_mods() & MOD_BIT(KC_RCTL);
     switch (keycode) {
         case RESET:
             if (record->event.pressed) {
                 rgblight_setrgb(0x00, 0x00, 0xFF);
             }
             return true;
-        case KC_BSDEL:
-            // LShift BS = DEL
+        case KC_BSDL:
+            // Shift-BS = DEL
             if (record->event.pressed) {
-                bsdel_shifted = shift_pressed;
-                if (shift_pressed) unregister_mods(MOD_LSFT);
-                register_code(shift_pressed ? KC_DEL : KC_BSPC);
+                mods_bsdl[0] = lsft_pressed;
+                mods_bsdl[1] = rsft_pressed;
+                if (lsft_pressed) unregister_mods(MOD_LSFT);
+                if (rsft_pressed) unregister_mods(MOD_RSFT);
+                register_code((mods_bsdl[0] || mods_bsdl[1]) ? KC_DEL : KC_BSPC);
             } else {
-                if (bsdel_shifted) register_mods(MOD_LSFT);
-                unregister_code(bsdel_shifted ? KC_DEL : KC_BSPC);
+                if (mods_bsdl[0]) register_mods(MOD_LSFT);
+                if (mods_bsdl[1]) register_mods(MOD_RSFT);
+                unregister_code((mods_bsdl[0] || mods_bsdl[1]) ? KC_DEL : KC_BSPC);
+                mods_bsdl[0] = false;
+                mods_bsdl[1] = false;
             }
             return false;
-        case KC_ESCGR:
+        case KC_ESGR:
             if (record->event.pressed) {
-                // LShift-Esc = TILD, RCtrl-Esc  = GRV
-                escgr_shifted = shift_pressed;
-                escgr_ctrled  = ctrl_pressed;
-                if (ctrl_pressed) unregister_mods(MOD_RCTL);
-                register_code((shift_pressed || ctrl_pressed) ? KC_GRV : KC_ESC);
+                // S-Esc = TILD(S-GRV)
+                // C-Esc = GRV
+                mods_esgr[0] = lsft_pressed;
+                mods_esgr[1] = rsft_pressed;
+                mods_esgr[2] = lctl_pressed;
+                mods_esgr[3] = rctl_pressed;
+                if (lctl_pressed || rctl_pressed) {
+                    unregister_mods(lctl_pressed ? MOD_LCTL : MOD_RCTL);
+                    register_code(KC_GRV);
+                } else if (rsft_pressed || lsft_pressed) {
+                    register_code(KC_GRV);
+                } else {
+                    register_code(KC_ESC);
+                }
             } else {
-                unregister_code((escgr_shifted || escgr_ctrled) ? KC_GRV : KC_ESC);
-                if (escgr_ctrled) register_mods(MOD_RCTL);
+                if (mods_esgr[2] || mods_esgr[3]) {
+                    unregister_code(KC_GRV);
+                    register_mods(mods_esgr[2] ? MOD_LCTL : MOD_RCTL);
+                } else if (mods_esgr[0] || mods_esgr[1]) {
+                    unregister_code(KC_GRV);
+                } else {
+                    unregister_code(KC_ESC);
+                }
+                mods_esgr[0] = false;
+                mods_esgr[1] = false;
+                mods_esgr[2] = false;
+                mods_esgr[3] = false;
             }
             return false;
-        case KC_M_F12:
+        case KC_MF12:
+            // S-F12 = F11
+            // C-F12 = F13
             if (record->event.pressed) {
-                if (ctrl_pressed) {
-                    unregister_mods(MOD_RCTL);
+                if (rctl_pressed || lctl_pressed) {
+                    unregister_mods(rctl_pressed ? MOD_RCTL : MOD_LCTL);
                     register_code(KC_F13);
                     unregister_code(KC_F13);
-                    register_mods(MOD_RCTL);
-                } else if (shift_pressed) {
-                    unregister_mods(MOD_LSFT);
+                    register_mods(rctl_pressed ? MOD_RCTL : MOD_LCTL);
+                } else if (lsft_pressed || rsft_pressed) {
+                    unregister_mods(lsft_pressed ? MOD_LSFT : MOD_RSFT);
                     register_code(KC_F11);
                     unregister_code(KC_F11);
-                    register_mods(MOD_LSFT);
+                    register_mods(lsft_pressed ? MOD_LSFT : MOD_RSFT);
                 } else {
                     register_code(KC_F12);
                     unregister_code(KC_F12);
@@ -160,30 +213,32 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             return false;
         case IME_ON:
-            // Ctrl-Shift-Ins
+            // IME ON = C-S-Ins
             if (record->event.pressed) {
                 ctrl_interrupted[0] = false;
                 scs_timer[0] = timer_read ();
-                register_mods(MOD_RCTL);
+                register_mods(MOD_LCTL);
             } else {
                 if (!ctrl_interrupted[0] && timer_elapsed(scs_timer[0]) < IME_TAPPING_TERM) {
-                    if (shift_pressed) {
-                        unregister_mods(MOD_LSFT);
+                    if (lsft_pressed || rsft_pressed) {
+                        // IME OFF
+                        unregister_mods(lsft_pressed ? MOD_LSFT : MOD_RSFT);
                         register_code(KC_INS);
                         unregister_code(KC_INS);
-                        register_mods(MOD_LSFT);
+                        register_mods(lsft_pressed ? MOD_LSFT : MOD_RSFT);
                     } else {
+                        // IME ON
                         register_mods(MOD_LSFT);
                         register_code(KC_INS);
                         unregister_code(KC_INS);
                         unregister_mods(MOD_LSFT);
                     }
                 }
-                unregister_mods(MOD_RCTL);
+                unregister_mods(MOD_LCTL);
             }
             return false;
         case IME_OFF:
-            // Ctrl-Ins, Shift-IME_OFF: Ctrl-Shift-Ins(IME_ON)
+            // IME OFF = C-INs
             if (record->event.pressed) {
                 ctrl_interrupted[1] = false;
                 scs_timer[1] = timer_read ();
@@ -196,10 +251,25 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 unregister_mods(MOD_LCTL);
             }
             return false;
+        case IME_TG:
+            // IME Toggle = C-Space
+            if (record->event.pressed) {
+                ctrl_interrupted[2] = false;
+                scs_timer[2] = timer_read ();
+                register_mods(MOD_LCTL);
+            } else {
+                if (!ctrl_interrupted[2] && timer_elapsed(scs_timer[2]) < IME_TAPPING_TERM) {
+                    register_code(KC_SPC);
+                    unregister_code(KC_SPC);
+                }
+                unregister_mods(MOD_LCTL);
+            }
+            return false;
 
         default:
             ctrl_interrupted[0] = true;
             ctrl_interrupted[1] = true;
+            ctrl_interrupted[2] = true;
 
             return true;
     }
